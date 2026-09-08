b1ec989c4db267332f27de08c58668f4cf362e75
QAnh <quanganhqpdb@gmail.com>
2026-09-08 09:11:58 +0700

    feat: them API counter va API info

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
+import os
 from flask import Flask
 import datetime, platform
+
 app = Flask(__name__)
+
+visit_count = 0 # Bien dem luu trong RAM cua container
+
+@app.route("/api/counter")
+def counter():
+    global visit_count
+    visit_count += 1
+    return {
+        "so_lan_truy_cap": visit_count,
+        "ghi_chu": "So nay se MAT khi container khoi dong lai!"
+    }
+
+@app.route("/api/info")
+def info():
+    ten_sinh_vien = os.environ.get("STUDENT_NAME", "Chua dat bien moi truong")
+    return {
+    "sinh_vien": ten_sinh_vien,
+    "nguon_du_lieu": "Environment Variable tren Render, KHONG hardcode trong code"
+    }
+
 @app.route("/")
 def home():
     return f"""
@@ -23,8 +45,10 @@ def home():
     <p>Developer chi viet code – PaaS lo build, deploy, HTTPS, scaling!</p>
     </body></html>
     """
+
 @app.route("/health")
 def health():
     return {"status": "ok"}
+
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
